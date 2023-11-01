bd9173c7780c3f39ec00163d5d2d3d8fac162f1d
sksuraj6077 <surajkawathale0122@gmail.com>
2023-11-01 11:51:08 +0530

    new docker image created

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . .
-
+EXPOSE 9001
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def predict_datapoint():
 
 
 if __name__=="__main__":
-    app.run(debug=True)        
+    app.run(host="0.0.0.0",port=5000,debug=True)        
 
         
         
